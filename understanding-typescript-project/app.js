0c7047c80eca03949b512f3ab1cccc4d704a0394
Conner <conner.chappell1123@gmail.com>
2022-05-17 15:18:07 -0600

    Unknown type

--- understanding-typescript-project/app.js
+++ understanding-typescript-project/app.js
@@ -1,19 +1,3 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-function addAndHanlde(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-// combineValues = printResult
-// combineValues = 5
-console.log(combineValues(8, 8));
-addAndHanlde(10, 20, function (result) {
-    console.log(result);
-});
+var userInput;
+userInput = 5;
+userInput = 'Conner';
